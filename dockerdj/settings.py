4391fae95d39e215edac3690ab576a0df46351f0
GhilaniYassine <ghilaniyassine11@gmail.com>
2025-09-01 10:45:11 +0100

    changing the settings keep secretes secret

--- dockerdj/settings.py
+++ dockerdj/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ccupo&%b@m)#4s^j+g_187_713hw#bgc9f-pa!$#j=k*rdo#s'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# The secret key
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ 
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+ 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
